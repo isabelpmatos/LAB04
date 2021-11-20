e7ed331e5240f97566a54649b3b412401c145b57
aquin0 <daniel.aquino@sga.pucminas.br>
2021-11-20 12:05:20 -0300

    empresaCrud front inicio

--- IMPLEMENTACAO/index.js
+++ IMPLEMENTACAO/index.js
@@ -22,8 +22,24 @@ app.get("/", (req, res) => {
     res.render("home")
 })
 
+app.get("/home", (req, res) => {
+    res.render("home");
+})
+
 app.get("/empresasParceiras", (req, res) =>{
     res.render("empresasParceiras")
+})
+
+app.get("/adicionarEmpresa", (req, res) =>{
+    res.render("adicionarEmpresa");
+})
+
+app.get("/editarEmpresa", (req, res) => {
+    res.render("editarEmpresa");
+})
+
+app.get("/consultarEmpresa", (req, res) =>{
+    res.render("consultarEmpresa");
 })
 
 app.listen(5002, () => {
